d4b9c2add4f5c0f0ff5f7f335a4f9562b098dccf
Andrey Trubilo <andreytrubilo@mail.ru>
2024-02-04 16:44:08 +0300

    Minor bag fix

--- Car-sales-department/src/main/java/andrey/code/api/service/CarOrderService.java
+++ Car-sales-department/src/main/java/andrey/code/api/service/CarOrderService.java
@@ -2,12 +2,10 @@ package andrey.code.api.service;
 
 import andrey.code.api.dto.AckDTO;
 import andrey.code.api.dto.CarOrderDTO;
-import andrey.code.store.entity.CarEntity;
 import andrey.code.store.entity.CarOrderEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
